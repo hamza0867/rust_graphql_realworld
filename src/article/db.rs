c446599da53389c41c9900f03ae2210d687d68a6
Hamza Benkhaldoun <hamza0867@gmail.com>
2021-10-23 22:51:28 +0100

    feat (article): implement delete

--- src/article/db.rs
+++ src/article/db.rs
@@ -1,16 +1,16 @@
-use diesel::result::QueryResult;
-use diesel::prelude::*;
-use crate::db_schema::articles ;
-use crate::db_schema::tags;
-use crate::db_schema::tag_article;
-use crate::db_schema::user_favorites_article;
-use crate::db::DbPool;
-use diesel::pg::upsert::*;
-use chrono::{DateTime, Utc};
 use super::resolvers::NewArticle;
+use crate::db::DbPool;
+use crate::db_schema::articles;
+use crate::db_schema::tag_article;
+use crate::db_schema::tags;
+use crate::db_schema::user_favorites_article;
+use chrono::{DateTime, Utc};
+use diesel::pg::upsert::*;
+use diesel::prelude::*;
+use diesel::result::QueryResult;
 use slugify::slugify;
 
-#[derive(Queryable, PartialEq, Debug )]
+#[derive(Queryable, PartialEq, Debug)]
 pub struct ArticleEntity {
     pub id: i32,
     pub slug: String,
@@ -19,20 +19,20 @@ pub struct ArticleEntity {
     pub body: String,
     pub created_at: DateTime<Utc>,
     pub updated_at: DateTime<Utc>,
-    pub author_id: i32
+    pub author_id: i32,
 }
 
 #[derive(Queryable, PartialEq, Insertable)]
 #[table_name = "tags"]
 pub struct TagEntity {
-    tag: String
+    tag: String,
 }
 
 #[derive(Queryable, PartialEq, Insertable)]
 #[table_name = "tag_article"]
 pub struct TagArticleEntity {
     tag: String,
-    article_id: i32
+    article_id: i32,
 }
 
 #[derive(Queryable, PartialEq, Insertable)]
@@ -40,9 +40,8 @@ pub struct TagArticleEntity {
 pub struct UserFavoritesArticle {
     article_id: i32,
     user_id: i32,
-    active: bool
+    active: bool,
 }
-
 
 #[derive(Insertable)]
 #[table_name = "articles"]
@@ -53,7 +52,7 @@ pub struct NewArticleDTO {
     pub slug: String,
     pub author_id: i32,
     pub created_at: DateTime<Utc>,
-    pub updated_at: DateTime<Utc>
+    pub updated_at: DateTime<Utc>,
 }
 
 fn new_article_dto_from_new_article(new_article: &NewArticle, author_id: i32) -> NewArticleDTO {
@@ -64,145 +63,185 @@ fn new_article_dto_from_new_article(new_article: &NewArticle, author_id: i32) ->
         slug: slugify!(new_article.title.as_str()),
         author_id,
         created_at: Utc::now(),
-        updated_at: Utc::now()
+        updated_at: Utc::now(),
     }
 }
 
-pub fn create(pool: &DbPool, new_article: NewArticle, given_author_id: i32 ) -> QueryResult<ArticleEntity> {
-    use diesel::insert_into;
+pub fn create(
+    pool: &DbPool,
+    new_article: NewArticle,
+    given_author_id: i32,
+) -> QueryResult<ArticleEntity> {
     use crate::db_schema::articles::dsl::*;
+    use diesel::insert_into;
     let conn = pool.get().unwrap();
-    let created_article_entity = conn.transaction::<_,diesel::result::Error,_>(|| {
+    let created_article_entity = conn.transaction::<_, diesel::result::Error, _>(|| {
         let new_article_dto = new_article_dto_from_new_article(&new_article, given_author_id);
         let created_article_entity = insert_into(articles)
-        .values(&new_article_dto)
-        .get_result::<ArticleEntity>(&conn)?;
+            .values(&new_article_dto)
+            .get_result::<ArticleEntity>(&conn)?;
         let returned_article_id = created_article_entity.id;
         if let Some(tag_list) = &new_article.tag_list {
             use crate::db_schema::tags::dsl::*;
-            let insertable_tags: Vec<TagEntity> = tag_list.iter().map(|given_tag| {
-                TagEntity {
-                    tag: given_tag.to_owned()
-                }
-            }).collect();
-            insert_into(tags)
-            .values(&insertable_tags)
-            .on_conflict(on_constraint("tags_pkey"))
-            .do_nothing()
-            .execute(&conn)?;
-
-            let insertable_article_tags: Vec<TagArticleEntity> = tag_list.iter().map(|given_tag| {
-                TagArticleEntity{
+            let insertable_tags: Vec<TagEntity> = tag_list
+                .iter()
+                .map(|given_tag| TagEntity {
                     tag: given_tag.to_owned(),
-                    article_id: returned_article_id 
-                }
-            }).collect();
+                })
+                .collect();
+            insert_into(tags)
+                .values(&insertable_tags)
+                .on_conflict(on_constraint("tags_pkey"))
+                .do_nothing()
+                .execute(&conn)?;
+
+            let insertable_article_tags: Vec<TagArticleEntity> = tag_list
+                .iter()
+                .map(|given_tag| TagArticleEntity {
+                    tag: given_tag.to_owned(),
+                    article_id: returned_article_id,
+                })
+                .collect();
             use crate::db_schema::tag_article::dsl::*;
-            let result  =  insert_into(tag_article)
-            .values(&insertable_article_tags)
-            .execute(&conn);
+            let result = insert_into(tag_article)
+                .values(&insertable_article_tags)
+                .execute(&conn);
             if let Err(e) = result {
                 return Err(e);
-            } 
+            }
         }
         Ok(created_article_entity)
-        })?;
+    })?;
     Ok(created_article_entity)
 }
 
-pub fn get_user_favorites_article(pool: &DbPool, given_user_id: i32, given_article_id: i32) -> QueryResult<bool> {
+pub fn get_user_favorites_article(
+    pool: &DbPool,
+    given_user_id: i32,
+    given_article_id: i32,
+) -> QueryResult<bool> {
     let conn = pool.get().unwrap();
     use crate::db_schema::user_favorites_article::dsl::*;
-    let favorites = user_favorites_article.filter(
-            user_id.eq(given_user_id).and(article_id.eq(given_article_id))
-          ).select(active).first::<bool>(&conn);
+    let favorites = user_favorites_article
+        .filter(
+            user_id
+                .eq(given_user_id)
+                .and(article_id.eq(given_article_id)),
+        )
+        .select(active)
+        .first::<bool>(&conn);
     match favorites {
         Err(diesel::result::Error::NotFound) => {
-                use diesel::insert_into;
-                let exec_result = insert_into(user_favorites_article)
-                .values(&UserFavoritesArticle{
+            use diesel::insert_into;
+            let exec_result = insert_into(user_favorites_article)
+                .values(&UserFavoritesArticle {
                     user_id: given_user_id,
                     article_id: given_article_id,
-                    active: false
-                }).execute(&conn);
-                if let Err(e) = exec_result  {
-                    return Err(e);
-                }
-                Ok(false)
-        },
+                    active: false,
+                })
+                .execute(&conn);
+            if let Err(e) = exec_result {
+                return Err(e);
+            }
+            Ok(false)
+        }
         Ok(x) => Ok(x),
-        Err(y) => Err(y)
+        Err(y) => Err(y),
     }
 }
 
 pub fn get_by_slug(pool: &DbPool, given_slug: String) -> QueryResult<ArticleEntity> {
     use crate::db_schema::articles::dsl::*;
     let conn = pool.get().unwrap();
-    let entity = articles.filter(slug.eq(given_slug)).first::<ArticleEntity>(&conn)?;
+    let entity = articles
+        .filter(slug.eq(given_slug))
+        .first::<ArticleEntity>(&conn)?;
     Ok(entity)
 }
 
-use super::resolvers::{ArticlesPage, ArticlesOptions};
+use super::resolvers::{ArticlesOptions, ArticlesPage};
 pub fn get_articles(pool: &DbPool, options: ArticlesOptions) -> QueryResult<ArticlesPage> {
     let conn = pool.get().unwrap();
     use diesel::pg::Pg;
     let mut query = crate::db_schema::articles::table.into_boxed::<Pg>();
     if let Some(given_tag) = options.tag {
         use crate::db_schema::tag_article::dsl::*;
-        let article_ids = tag_article.filter(tag.eq(given_tag)).select(article_id).load::<i32>(&conn)?;
+        let article_ids = tag_article
+            .filter(tag.eq(given_tag))
+            .select(article_id)
+            .load::<i32>(&conn)?;
         use crate::db_schema::articles::dsl::*;
         query = query.filter(id.eq_any(article_ids));
     }
     if let Some(given_author) = options.author {
-        use crate::db_schema::users::dsl::*;
         use crate::db_schema::users::dsl::id;
-        let given_author_id = users.filter(username.eq(given_author)).select(id).first::<i32>(&conn)?;
+        use crate::db_schema::users::dsl::*;
+        let given_author_id = users
+            .filter(username.eq(given_author))
+            .select(id)
+            .first::<i32>(&conn)?;
         use crate::db_schema::articles::dsl::*;
         query = query.filter(author_id.eq(given_author_id));
     }
     if let Some(given_favorited_by) = options.favorited {
-        use crate::db_schema::users::dsl::*;
         use crate::db_schema::user_favorites_article::dsl::*;
-        let given_favorited_by_id = users.filter(username.eq(given_favorited_by)).select(id).first::<i32>(&conn)?;
-        let article_ids = user_favorites_article.filter(active.eq(true).and(user_id.eq(given_favorited_by_id))).select(article_id).load::<i32>(&conn)?;
+        use crate::db_schema::users::dsl::*;
+        let given_favorited_by_id = users
+            .filter(username.eq(given_favorited_by))
+            .select(id)
+            .first::<i32>(&conn)?;
+        let article_ids = user_favorites_article
+            .filter(active.eq(true).and(user_id.eq(given_favorited_by_id)))
+            .select(article_id)
+            .load::<i32>(&conn)?;
         query = query.filter(crate::db_schema::articles::dsl::id.eq_any(article_ids));
     }
     use crate::db_schema::articles::dsl::created_at;
-    let articles = query.offset(options.offset.unwrap_or(0) as i64)
-    .limit(options.limit.unwrap_or(20) as i64)
-    .order_by(created_at.asc())
-    .load::<ArticleEntity>(&conn)?;
+    let articles = query
+        .offset(options.offset.unwrap_or(0) as i64)
+        .limit(options.limit.unwrap_or(20) as i64)
+        .order_by(created_at.asc())
+        .load::<ArticleEntity>(&conn)?;
 
     let count = articles.len() as i32;
 
     Ok(ArticlesPage {
         articles,
-        articles_count: count
+        articles_count: count,
     })
 }
 
-use super::resolvers:: FeedOptions;
+use super::resolvers::FeedOptions;
 
-pub fn get_feed(pool: &DbPool, user_id: i32,options: FeedOptions) -> QueryResult<ArticlesPage> {
+pub fn get_feed(pool: &DbPool, user_id: i32, options: FeedOptions) -> QueryResult<ArticlesPage> {
     let conn = pool.get().unwrap();
 
     use crate::db_schema::articles::dsl::*;
     use crate::db_schema::follows::dsl::*;
 
     let followed_authors_ids = follows.filter(follower_id.eq(user_id)).select(followed_id);
-    let query = articles.filter(
-        author_id.eq_any(followed_authors_ids)
-    );
+    let query = articles.filter(author_id.eq_any(followed_authors_ids));
     use crate::db_schema::articles::dsl::created_at;
-    let found_articles = query.offset(options.offset.unwrap_or(0) as i64)
-    .limit(options.limit.unwrap_or(20) as i64)
-    .order_by(created_at.asc())
-    .load::<ArticleEntity>(&conn)?;
+    let found_articles = query
+        .offset(options.offset.unwrap_or(0) as i64)
+        .limit(options.limit.unwrap_or(20) as i64)
+        .order_by(created_at.asc())
+        .load::<ArticleEntity>(&conn)?;
 
     let count = found_articles.len() as i32;
 
     Ok(ArticlesPage {
         articles: found_articles,
-        articles_count: count
+        articles_count: count,
     })
+}
+
+pub fn delete(pool: &DbPool, given_id: i32) -> QueryResult<()> {
+    let conn = pool.get().unwrap();
+
+    use crate::db_schema::articles::dsl::*;
+
+    diesel::delete(articles.filter(id.eq(given_id))).execute(&conn)?;
+
+    Ok(())
 }
--- src/article/resolvers.rs
+++ src/article/resolvers.rs
@@ -1,11 +1,8 @@
-use juniper::{FieldResult, GraphQLInputObject, IntoFieldError,
-GraphQLObject};
+use juniper::{FieldResult, GraphQLInputObject, GraphQLObject, IntoFieldError};
 
+use super::db::ArticleEntity;
 use crate::schema::Context;
 use crate::user::auth;
-use super::db::ArticleEntity;
-
-
 
 #[derive(GraphQLInputObject)]
 #[graphql(description = "Payload to create an article")]
@@ -13,6 +10,13 @@ pub struct NewArticle {
     pub title: String,
     pub description: Option<String>,
     pub body: String,
+    pub tag_list: Option<Vec<String>>,
+}
+
+pub struct UpdateArticle {
+    pub title: Option<String>,
+    pub description: Option<String>,
+    pub body: Option<String>,
     pub tag_list: Option<Vec<String>>,
 }
 
@@ -31,28 +35,60 @@ impl ArticleMutation {
         let article = create(pool, new_article, author_id)?;
         Ok(article)
     }
+
+    //fn update_article(
+    //context: &Context,
+    //update_article: UpdateArticle,
+    //) -> FieldResult<ArticleEntity> {
+    //use super::db::create;
+    //let pool = &context.db_pool;
+    //let id = auth::get_id_from_token(&context.token);
+    //if let Err(e) = id {
+    //return Err(e);
+    //};
+    //let author_id = id.unwrap();
+    //let article = create(pool, update_article, author_id)?;
+    //Ok(article)
+    //}
+
+    fn delete_article(context: &Context, article_slug: String) -> FieldResult<String> {
+        use super::db::delete;
+        let pool = &context.db_pool;
+        let id = auth::get_id_from_token(&context.token);
+        if let Err(e) = id {
+            return Err(e);
+        };
+        let author_id = id.unwrap();
+        use super::db::get_by_slug;
+        let article = get_by_slug(pool, article_slug)?;
+        if article.author_id != author_id {
+            return Err(crate::user::errors::UserError::Unauthorized.into_field_error());
+        }
+        delete(pool, article.id)?;
+        Ok(article.slug)
+    }
 }
 
 #[derive(GraphQLInputObject)]
 pub struct ArticlesOptions {
-   pub tag: Option<String>,
-   pub author: Option<String>,
-   pub favorited: Option<String>,
-   pub limit: Option<i32>,
-   pub offset: Option<i32>
+    pub tag: Option<String>,
+    pub author: Option<String>,
+    pub favorited: Option<String>,
+    pub limit: Option<i32>,
+    pub offset: Option<i32>,
 }
 
 #[derive(GraphQLObject)]
 #[graphql(context = Context)]
 pub struct ArticlesPage {
-   pub articles: Vec<ArticleEntity>,
-   pub articles_count: i32
+    pub articles: Vec<ArticleEntity>,
+    pub articles_count: i32,
 }
 
 #[derive(GraphQLInputObject)]
 pub struct FeedOptions {
-   pub limit: Option<i32>,
-   pub offset: Option<i32>
+    pub limit: Option<i32>,
+    pub offset: Option<i32>,
 }
 
 pub struct ArticleQuery;
@@ -64,7 +100,9 @@ impl ArticleQuery {
         use super::db::get_by_slug;
         let article_result = get_by_slug(pool, slug);
         match article_result {
-            Err(diesel::result::Error::NotFound) => Err(super::errors::ArticleError::NotFound.into_field_error()),
+            Err(diesel::result::Error::NotFound) => {
+                Err(super::errors::ArticleError::NotFound.into_field_error())
+            }
             Ok(article) => Ok(article),
             Err(e) => {
                 eprintln!("{}", e);
@@ -76,7 +114,6 @@ impl ArticleQuery {
                     }),
                 ))
             }
-
         }
     }
 
@@ -96,11 +133,10 @@ impl ArticleQuery {
                     }),
                 ))
             }
-
         }
     }
 
-    fn feed(context: &Context, options: Option<FeedOptions > ) -> FieldResult<ArticlesPage> {
+    fn feed(context: &Context, options: Option<FeedOptions>) -> FieldResult<ArticlesPage> {
         let id = auth::get_id_from_token(&context.token);
         if let Err(e) = id {
             return Err(e);
@@ -108,10 +144,10 @@ impl ArticleQuery {
         let user_id = id.unwrap();
 
         let pool = &context.db_pool;
-        
+
         let feed_options = options.unwrap_or(FeedOptions {
             limit: None,
-            offset: None
+            offset: None,
         });
 
         use super::db::get_feed;
@@ -128,8 +164,6 @@ impl ArticleQuery {
                     }),
                 ))
             }
-
         }
     }
 }
-

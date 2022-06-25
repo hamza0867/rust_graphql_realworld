60c5abae2a2c3410195bbcc482fd12cbf23dc869
Hamza Benkhaldoun <hamza0867@gmail.com>
2022-06-25 23:42:58 +0100

    chore: update dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,59 +1,58 @@
 #[macro_use]
 extern crate diesel;
 extern crate dotenv;
-extern crate slugify;
 extern crate juniper;
+extern crate slugify;
 
-use actix_web::{ middleware, web, App, Error, HttpResponse, HttpServer};
+use actix_web::{
+    middleware,
+    web::{self, Data},
+    App, Error, HttpResponse, HttpServer,
+};
 use actix_web_httpauth::extractors::bearer::BearerAuth;
 use db::DbPool;
-use juniper::http::playground::playground_source;
-use juniper::http::GraphQLRequest;
+use juniper_actix::{graphiql_handler, graphql_handler, playground_handler};
 use schema::Context;
 
+mod article;
 mod db;
 mod db_schema;
 mod schema;
 mod user;
-mod article;
 
 use crate::schema::{create_schema, Schema};
 
 pub async fn graphql(
+    req: actix_web::HttpRequest,
+    payload: actix_web::web::Payload,
     pool: web::Data<DbPool>,
     schema: web::Data<Schema>,
-    data: web::Json<GraphQLRequest>,
     credentials: Option<BearerAuth>,
 ) -> Result<HttpResponse, Error> {
-    let res = web::block(move || {
-        let ctx = Context {
-            db_pool: pool.get_ref().to_owned(),
-            token: credentials.map(|auth| {
-                auth.token().to_string()
-            }) ,
-        };
-        let res = data.execute_sync(&schema, &ctx);
-        Ok::<_, serde_json::error::Error>(serde_json::to_string(&res)?)
-    })
-    .await
-    .map_err(Error::from)?;
-
-    Ok(HttpResponse::Ok()
-    .content_type("application/json")
-    .body(res))
+    let ctx = Context {
+        db_pool: pool.get_ref().to_owned(),
+        token: credentials.map(|auth| auth.token().to_string()),
+    };
+    graphql_handler(&schema, &ctx, req, payload).await
 }
 
-pub async fn graphql_playground() -> HttpResponse {
-    HttpResponse::Ok()
-    .content_type("text/html; charset=utf-8")
-    .body(playground_source("/graphql", None))
+async fn graphiql_route() -> Result<HttpResponse, Error> {
+    graphiql_handler("/graphql", None).await
+}
+async fn playground_route() -> Result<HttpResponse, Error> {
+    playground_handler("/graphql", None).await
 }
 
 pub fn register(config: &mut web::ServiceConfig) {
     config
-    .data(create_schema())
-    .route("/graphql", web::post().to(graphql))
-    .route("/graphiql", web::get().to(graphql_playground));
+        .app_data(Data::new(create_schema()))
+        .service(
+            web::resource("/graphql")
+                .route(web::post().to(graphql))
+                .route(web::get().to(graphql)),
+        )
+        .service(web::resource("/playground").route(web::get().to(playground_route)))
+        .service(web::resource("/graphiql").route(web::get().to(graphiql_route)));
 }
 
 #[actix_web::main]
@@ -61,13 +60,12 @@ async fn main() -> std::io::Result<()> {
     let db_pool = db::get_db_pool();
     HttpServer::new(move || {
         App::new()
-        .data(db_pool.clone())
-        .wrap(middleware::Logger::default())
-        .configure(register)
-        .default_service(web::to(|| async { "404" }))
+            .app_data(Data::new(db_pool.clone()))
+            .wrap(middleware::Logger::default())
+            .configure(register)
+            .default_service(web::to(|| async { "404" }))
     })
     .bind("127.0.0.1:8080")?
     .run()
     .await
 }
-
